22134ca5ffe57d131dc9f7c504ba9a23cce7513f
Ratheepan <ratheepan93@hotmail.com>
2024-02-08 22:12:31 +0530

    8-7-2024

---
+++ js/conditional-statements.js
@@ -0,0 +1,69 @@
+// var mark = 50;
+
+// var result = "N/A";
+
+// if (mark >= 0 && mark <= 25) {
+//   result = "F";
+// } else if (mark > 25 && mark <= 45) {
+//   result = "W";
+// } else if (mark > 45 && mark <= 65) {
+//   result = "C";
+// } else if (mark > 65 && mark <= 75) {
+//   result = "B";
+// } else if (mark > 75 && mark <= 100) {
+//   result = "A";
+// } else {
+//   result = "Wrong Mark";
+// }
+
+// console.log(result);
+
+// var i = 0;
+// while (i <= 10) {
+//   console.log("hello", i);
+//   i++;
+// }
+
+// var i = 0;
+// do {
+//   console.log("hello", i);
+//   i++;
+// } while (i <= 10);
+
+// 11
+// for (var i = 0; i <= 10; i++) {
+//   // if (i == 6) {
+//   // continue;
+//   //   break;
+//   // }
+//   console.log("hello", i);
+// }
+
+var marks = [50, 45, 78, 45, 85];
+// for (let i = 0; i < marks.length; i++) {
+//   console.log(marks[i]);
+// }
+
+var users = [
+  {
+    name: "Raja",
+    age: 20,
+    city: "Jaffna",
+  },
+  {
+    name: "Vimal",
+    age: 45,
+    city: "Mannar",
+  },
+];
+
+// for (i in users) {
+//   let user = users[i];
+//   console.log(user["name"]);
+// }
+
+for (user of users) {
+  console.log(user.name);
+}
+
+// console.log(marks.length);
--- js/my-script.js
+++ js/my-script.js
@@ -1,3 +1,77 @@
-const greeting = 1000;
+// Array
+// let greeting = [1, 2, 3, 4, "Apple", [10, 11]];
+// let value = greeting[5][0];
+// console.log(value);
 
-console.log(greeting);
+// var number1 = 5;
+// var number2 = 10;
+
+// var number1 = number1 - 1;
+
+// var value = number1 ** 3;
+//  5 * 5 * 5
+
+// number1 = number1 + 7;
+// number1 += 7;
+
+// number1 = number1 - 3;
+// number1 -= 3;
+
+// console.log(number1);
+
+// Single line comment
+
+/**
+ * Multi
+ * line
+ * comment
+ */
+
+// var var1 = true;
+// var var2 = false;
+
+// var value = !var2;
+
+// console.log(value);
+
+// Comparison operators
+
+// var number1 = 10;
+// var number2 = 10;
+// var equ = "No";
+
+// if (number1 == number2) {
+//   equ = "Yes";
+// }
+
+// var equ = number1 == number2 ? "Yes" : "No";
+
+// console.log(equ);
+
+// var user = {
+//   name: "Raja",
+//   schools: {
+//     name: "ABC",
+//   },
+// };
+
+// console.log(user.schools?.name);
+
+// var num1 = "asd";
+// var num2 = 20;
+// var value = 0;
+
+// var user = {
+//   name: "Raja",
+//   schools: {
+//     name: "ABC",
+//   },
+// };
+
+// user.name = "Vimal";
+
+// var user2 = user;
+
+// console.log(user2 instanceof user);
+
+console.log(5 & 1);
